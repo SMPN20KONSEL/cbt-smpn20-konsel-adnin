ac2a59610c2a0d650ae66b4d7263e800f80e6844
SMPN20KONSEL <EMAIL_GITHUB_SMPN20KONSEL>
2026-04-04 10:52:38 +0800

    update

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,32 +1,49 @@
+// assets/js/dashboard.js
 import { db } from "./firebase.js";
 import { collection, onSnapshot } 
 from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
-/* ================= TOTAL SISWA ================= */
-onSnapshot(collection(db, "siswa"), (snapshot) => {
-  document.getElementById("totalSiswa").innerText = snapshot.size;
+/* ================= TOTAL ================= */
+onSnapshot(collection(db, "siswa"), (snap) => {
+  document.getElementById("totalSiswa").innerText = snap.size;
 });
 
-/* ================= TOTAL UJIAN ================= */
-onSnapshot(collection(db, "ujian"), (snapshot) => {
-  document.getElementById("totalUjian").innerText = snapshot.size;
+onSnapshot(collection(db, "guru"), (snap) => {
+  document.getElementById("totalguru").innerText = snap.size;
 });
-
-/* ================= MONITORING REALTIME ================= */
-const table = document.getElementById("monitoringTable");
-
-onSnapshot(collection(db, "absensi_ujian"), (snapshot) => {
+// ================= MONITORING =================
+onSnapshot(collection(db, "jawaban_siswa"), (snapshot) => {
   table.innerHTML = "";
 
   snapshot.forEach((doc) => {
     const data = doc.data();
 
+    let statusText = "";
+    let className = "";
+    let waktu = "-";
+
+    // ================= STATUS =================
+    if (!data.waktu_mulai) {
+      statusText = "Belum Mengerjakan";
+      className = "belum";
+    } 
+    else if (data.waktu_mulai && !data.waktu_selesai) {
+      statusText = "Sedang Mengerjakan";
+      className = "aktif";
+      waktu = new Date().toLocaleTimeString();
+    } 
+    else if (data.waktu_selesai) {
+      statusText = "Selesai";
+      className = "selesai";
+      waktu = new Date(data.waktu_selesai.seconds * 1000).toLocaleTimeString();
+    }
+
     table.innerHTML += `
       <tr>
-        <td>${data.nama}</td>
-        <td>${data.kelas}</td>
-        <td>${data.status}</td>
-        <td>${data.waktu}</td>
+        <td>${data.namaSiswa}</td>
+        <td>${data.kelas || "-"}</td>
+        <td class="${className}">${statusText}</td>
+        <td>${waktu}</td>
       </tr>
     `;
   });
